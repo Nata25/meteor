cfa312dfb7b9906a7e9222286dc1d5eff904e335
nataivanova <natalyas.ivanova@gmail.com>
2016-09-21 00:34:36 +0300

    Display time the comment was posted. Fix heading

--- siteace.js
+++ siteace.js
@@ -46,17 +46,17 @@ if (Meteor.isClient) {
 	// helper function that returns all available websites
 	Template.website_list.helpers({
 		websites:function(){
-			return Websites.find({}, {sort:{rating:-1}});
+			return Websites.find({}, {sort:{rating:-1, createdOn:-1}});
 		}
 	});
 
-	Template.body.helpers({
+	Template.addDate.helpers({
 		formattedDate:function() {
 			var monthNames = [
-		  "January", "February", "March",
-		  "April", "May", "June", "July",
-		  "August", "September", "October",
-		  "November", "December"
+		  "Jan", "Feb", "Mar",
+		  "Apr", "May", "June", "July",
+		  "Aug", "Sept", "Oct",
+		  "Nov", "Dec"
 		];
 			var createdOn = this.createdOn;
 			var monthIndex = createdOn.getMonth();
@@ -153,34 +153,56 @@ if (Meteor.isClient) {
 			var comment = event.target.comment.value;
 			var website_id = this._id;
 			event.target.comment.value = '';
-			console.log("submitting comment by", username);
-			console.log("here's the comment:", comment);
-			console.log("website's id is", website_id);
 
 			Comments.insert(
 				{
 				website_id: website_id,
 				user: username,
 				text: comment,
-				added: new Date()
+				posted: new Date(2016, 8, 20)
 				}
 			);
-			console.log("Total number of comments in database:", Comments.find().count());
-			var lastComment = Comments.findOne({user: "nata"});
-			console.log("last comment: ", lastComment);
-			console.log("last comment was left by ", lastComment.user);
 
 			return false;
 		}
 	});
 
 	Template.website_comments.helpers({
+		already_commented:function() {
+			return Comments.findOne({website_id: this._id});
+		},
+
 		display_comments:function(){
-			console.log("inside display_comments");
-			if (Comments.find().count() != 0) {
-				console.log("hello!!!");
-				return Comments.find({}, {sort:{added:-1}});
+			return Comments.find({website_id: this._id}, {sort:{posted:-1}});
+		},
+       // indicate time when the comment was posted
+		time:function() {
+			var now, posted, diff, yearInMiliseconds, monthInMiliseconds, dayInMilisecond, hourInMiliseconds;
+			now = new Date(2016, 8, 21);
+			posted = this.posted;
+			diff = now - posted;
+			yearInMiliseconds = 31556926000;
+			dayInMiliseconds = 86400000;
+			hourInMiliseconds = 3600000;
+			// if the difference is greater than 1 year, return year diff
+			if (diff > yearInMiliseconds) {
+				return Math.round(diff / yearInMiliseconds) + " year(s) ago";
 			}
+			// else calculate month difference
+			monthInMiliseconds = yearInMiliseconds / 12;
+			if (diff > monthInMiliseconds) {
+				return Math.round(diff / monthInMiliseconds) + " month(s) ago";
+			}
+			// if more than two day passed, calculate difference in days
+			if (diff > dayInMiliseconds * 2) {
+				return Math.round(diff / dayInMiliseconds) + " days ago";
+			}
+			// if it was posted within a day
+			if (diff > hourInMiliseconds) {
+				return "about a day ago";
+			}
+			// just now option
+			return "just now";
 		}
 	});
 
@@ -197,7 +219,7 @@ if (Meteor.isServer) {
     		title:"Goldsmiths Computing Department",
     		url:"http://www.gold.ac.uk/computing/",
     		description:"This is where this course was developed.",
-    		createdOn:new Date(),
+    		createdOn:new Date(2015, 1, 11),
 			upvotes:0,
 			downvotes:0
     	});
@@ -205,7 +227,7 @@ if (Meteor.isServer) {
     		title:"University of London",
     		url:"http://www.londoninternational.ac.uk/courses/undergraduate/goldsmiths/bsc-creative-computing-bsc-diploma-work-entry-route",
     		description:"University of London International Programme.",
-    		createdOn:new Date(),
+    		createdOn:new Date(2016, 2, 2),
 			upvotes:0,
 			downvotes:0
     	});
@@ -213,7 +235,7 @@ if (Meteor.isServer) {
     		title:"Coursera",
     		url:"http://www.coursera.org",
     		description:"Universal access to the world’s best education.",
-    		createdOn:new Date(),
+    		createdOn:new Date(2010, 8, 1),
 			upvotes:0,
 			downvotes:0
     	});
