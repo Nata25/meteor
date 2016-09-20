6506679ccc0e5732aa7eddf7e4908a631ec249dc
nataivanova <natalyas.ivanova@gmail.com>
2016-09-21 02:06:47 +0300

    Reorganize folders

--- client/siteace.js
+++ client/siteace.js
@@ -1,9 +1,3 @@
-Websites = new Mongo.Collection("websites");
-Comments = new Mongo.Collection("comments");
-
-
-if (Meteor.isClient) {
-
 	/// routing
 
 	Router.configure({
@@ -220,48 +214,3 @@ if (Meteor.isClient) {
 			return "just now";
 		}
 	});
-
-}
-
-
-if (Meteor.isServer) {
-	// start up function that creates entries in the Websites databases.
-  Meteor.startup(function () {
-    // code to run on server at startup
-    if (!Websites.findOne()){
-    	console.log("No websites yet. Creating starter data.");
-    	  Websites.insert({
-    		title:"Goldsmiths Computing Department",
-    		url:"http://www.gold.ac.uk/computing/",
-    		description:"This is where this course was developed.",
-    		createdOn:new Date(2015, 1, 11),
-			upvotes:0,
-			downvotes:0
-    	});
-    	 Websites.insert({
-    		title:"University of London",
-    		url:"http://www.londoninternational.ac.uk/courses/undergraduate/goldsmiths/bsc-creative-computing-bsc-diploma-work-entry-route",
-    		description:"University of London International Programme.",
-    		createdOn:new Date(2016, 2, 2),
-			upvotes:0,
-			downvotes:0
-    	});
-    	 Websites.insert({
-    		title:"Coursera",
-    		url:"http://www.coursera.org",
-    		description:"Universal access to the world’s best education.",
-    		createdOn:new Date(2010, 8, 1),
-			upvotes:0,
-			downvotes:0
-    	});
-    	Websites.insert({
-    		title:"Google",
-    		url:"http://www.google.com",
-    		description:"Popular search engine.",
-    		createdOn:new Date(),
-			upvotes:0,
-			downvotes:0
-    	});
-    }
-  });
-}
